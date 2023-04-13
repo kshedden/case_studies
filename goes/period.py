20ec8f42bbd58ed0070521b1d46889e8f198e8d9
Kerby Shedden <kshedden@umich.edu>
2023-04-13 14:13:31 -0400

    update

--- goes/period.py
+++ goes/period.py
@@ -6,17 +6,21 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 df = get_goes(2017)
 
-# Arange the data into blocks containing 1000 observations, which
-# is around 33 minutes of data.
-tix, flx = make_blocks(df, 5000, 0)
+pdf = PdfPages("period_py.pdf")
 
-def make_plot(w, ma):
+# Arange the data into blocks containing mp observations, which
+# is around mp/30 minutes of data.
+mp = 5000
+tix, flx = make_blocks(df, mp, 0)
+
+def make_plot(w, ma, ti):
     plt.clf()
     plt.grid(True)
     for m in ma:
         plt.plot(60*w, m)
     plt.ylabel("Energy", size=15)
     plt.xlabel("Cycles/minute (frequency)", size=15)
+    plt.title(ti)
     pdf.savefig()
     plt.clf()
     plt.grid(True)
@@ -24,6 +28,7 @@ def make_plot(w, ma):
         plt.plot(1/(60*w), m)
     plt.ylabel("Energy", size=15)
     plt.xlabel("Minutes/cycle (period)", size=15)
+    plt.title(ti)
     pdf.savefig()
 
 # Test data to be sure we are interpreting the plot correctly.  This data
@@ -32,11 +37,12 @@ def make_plot(w, ma):
 period = 10     # True period
 w = 1 / period  # True frequency
 ti = np.sort(1000*np.random.uniform(size=100000))
-flux1 = np.cos(w*2*np.pi*ti) + 0.1*np.random.normal(size=ti.shape[0])
-w = np.linspace(0.1, 2, 400)  # Frequencies to fit
-m = lombscargle(ti, flux1, w, precenter=True, normalize=True)
-ww = w / (2*np.pi)
-make_plot(ww, [m])
+for s in [0.5, 10, 50, 100]:
+    flux1 = np.cos(w*2*np.pi*ti) + s*np.random.normal(size=ti.shape[0])
+    wf = np.linspace(0.1, 2, 400)  # Frequencies to fit
+    m = lombscargle(ti, flux1, wf, precenter=True, normalize=True)
+    ww = wf / (2*np.pi)
+    make_plot(ww, [m], "Simulated data (s=%.2f)" % s)
 
 ma = []
 w = np.linspace(0.01, 0.5, 800)  # Frequencies to fit
@@ -49,13 +55,12 @@ for k in range(flx.shape[0]):
 
 ma = np.asarray(ma)
 
-pdf = PdfPages("period_py.pdf")
-
 plt.clf()
 plt.grid(True)
 plt.plot(60*ww, ma.mean(0))
 plt.xlabel("Cycles/minute (frequency)", size=15)
 plt.ylabel("Energy", size=15)
+plt.title("GOES-1")
 pdf.savefig()
 
 lma = np.log(ma)
@@ -72,6 +77,7 @@ plt.plot(60*ww, lmn + f*v[:, 0], "-", color="grey")
 plt.plot(60*ww, lmn - f*v[:, 0], "-", color="grey")
 plt.xlabel("Cycles/minute (frequency)", size=15)
 plt.ylabel("Log energy", size=15)
+plt.title("GOES-1")
 pdf.savefig()
 
 pdf.close()
