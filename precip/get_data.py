431c0226ce96239a03d45925d7ab55411d26d646
kshedden <kshedden@umich.edu>
2023-09-12 16:17:32 -0400

    update

--- precip/get_data.py
+++ precip/get_data.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 
+# Download the NCDC data, also available from this site:
+#  https://www.ncei.noaa.gov/access/search/data-search/daily-summaries
+
 base_url = "https://www.ncei.noaa.gov/data/daily-summaries/access"
 
 target_dir = "/home/kshedden/data/Teaching/precip"
@@ -10,4 +13,3 @@ files = ["USW00094847.csv", "USW00012839.csv",]
 for f in files:
     df = pd.read_csv(os.path.join(base_url, f))
     df.to_csv(os.path.join(target_dir, f + ".gz"))
-
