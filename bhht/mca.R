bedf2e1e5f5b708b4d8448d371ae2a8ffe444482
kshedden <kshedden@umich.edu>
2023-10-06 00:06:45 -0400

    update

--- bhht/mca.R
+++ bhht/mca.R
@@ -1,29 +1,26 @@
 library(dplyr)
 library(FactoMineR)
+library(tidyr)
 library(readr)
 
 # Change this as needed to point to the directory holding the data file.
 pa = "/home/kshedden/mynfs/data/Teaching/bhht"
 
 da = read_csv(file.path(pa, "cross-verified-database.csv.gz"))
-da = rename(da, "occ"="level1_main_occ")
-da = rename(da, "reg"="un_region")
-da = rename(da, "sex"="gender")
+da = da %>% rename(occ=level1_main_occ, reg=un_region, sex=gender)
 
-dx = da[, c("birth", "occ", "sex", "reg")]
-dx = dx[complete.cases(dx),]
-dx = dx[dx$birth >= 1500,]
+dx = da %>% select(birth, occ, sex, reg)
+dx = dx %>% drop_na()
+dx = dx %>% filter(birth >= 1500)
 
 # Create a "century of birth" variable
-dx$bcen = round(dx$birth, -2)
-dx = dx[, c("bcen", "occ", "sex", "reg")]
-dx$bcen = as.factor(dx$bcen)
+dx = dx %>% mutate(bcen = round(birth, -2))
+dx = dx %>% select(bcen, occ, sex, reg)
+dx = dx %>% mutate(bcen = as.factor(bcen))
 
 # Remove small groups that are difficult to interpret
 # due to low precision or high confounding.
-dx = filter(dx, occ != "Other")
-dx = filter(dx, occ != "Missing")
-dx = filter(dx, sex != "Other")
+dx = dx %>% filter(occ != "Other" & occ != "Missing" & sex != "Other")
 
 mm = MCA(dx, ncp=3, graph=FALSE)
 
@@ -31,7 +28,6 @@ pdf("bhht_r_mca.pdf")
 
 plt = plot(mm, axes=c(1, 2), invisible="ind")
 print(plt)
-dev.off()
 
 plt = plot(mm, axes=c(1, 3), invisible="ind")
 print(plt)
