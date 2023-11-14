5ad304fa116fbce4c10891f81a648861f369b172
kshedden <kshedden@umich.edu>
2023-11-14 14:19:51 -0500

    update

---
+++ inaturalist/largescale.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from pathlib import Path
+from scipy.stats.distributions import norm
+from statsmodels.stats.multitest import local_fdr
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+#pclass = "Pinopsida"
+pclass = "Polypodiopsida"
+
+pdf = PdfPages("%s_largescale.pdf" % pclass) 
+
+pa = Path("/home/kshedden/data/Teaching/inaturalist")
+
+fn = pa / ("Plantae_%s.csv.gz" % pclass)
+
+df = pd.read_csv(fn, parse_dates=["eventDate"])
+
+df["day"] = (df["eventDate"] - pd.to_datetime("2010-01-01")).dt.days
+
+# Treat longitude as a circular variable
+df["lonrad"] = np.pi * df["decimalLongitude"] / 180
+df["lonrad_sin"] = np.sin(df["lonrad"])
+df["lonrad_cos"] = np.cos(df["lonrad"])
+
+# Group by species and fit a linear model with OLS for each species.
+rr = []
+for (sp,dx) in df.groupby("species"):
+
+    if dx.shape[0] < 10:
+        continue
+
+    md = sm.OLS.from_formula("decimalLatitude ~ day + lonrad_sin + lonrad_cos", data=dx)
+    mr = md.fit()
+    rr.append([sp, dx.shape[0], mr.params["day"], mr.bse["day"]])
+
+rr = pd.DataFrame(rr, columns=["species", "n", "day_slope", "day_slope_se"])
+rr["day_slope_z"] = rr["day_slope"] / rr["day_slope_se"]
+
+# To control family-wise error rates using the Bonferroni approach,
+# the Z-scores must exceed this value in magnitude.
+bonf_z = norm.ppf(1 - 0.025 / rr.shape[0])
+
+# Local False Discovery Rate (FDR)
+rr["locfdr"] = local_fdr(rr["day_slope_z"])
+
+plt.clf()
+plt.grid(True)
+plt.plot(rr["day_slope"], rr["day_slope_z"], "o", alpha=0.5)
+plt.xlabel("Day slope", size=15)
+plt.ylabel("Day slope (Z)", size=15)
+pdf.savefig()
+
+plt.clf()
+plt.grid(True)
+plt.plot(rr["day_slope_z"], rr["locfdr"], "o", alpha=0.5)
+plt.xlabel("Day slope (Z)", size=15)
+plt.ylabel("Local FDR", size=15)
+pdf.savefig()
+
+plt.clf()
+plt.grid(True)
+plt.plot(np.log(rr["n"]), rr["day_slope_z"], "o", alpha=0.5)
+plt.xlabel("n", size=15)
+plt.ylabel("Day slope (Z)", size=15)
+pdf.savefig()
+
+pdf.close()
--- inaturalist/rslopes.R
+++ inaturalist/rslopes.R
@@ -7,6 +7,10 @@ library(ggplot2)
 library(ggrastr)
 library(ggfortify)
 library(mapdata)
+library(RLRsim)
+
+# Use multileel regression to assess for species-specific temporal trends in the
+# occurrences of species within a class of plants.
 
 pclass = "Pinopsida"
 #pclass = "Polypodiopsida"
@@ -35,17 +39,23 @@ plt = plt + theme(legend.position="none")
 plt = rasterize(plt, dpi=100)
 print(plt)
 
-# A multilevel linear regression with random intercepts. 
-m0 = lmer(decimalLatitude ~ (1 | species) + day + sin(lonrad) + cos(lonrad) + sin(lonrad/2) + cos(lonrad/2), da)
+# A multilevel linear regression with random species intercepts. 
+m1 = lmer(decimalLatitude ~ (1 | species) + day + sin(lonrad) + cos(lonrad) + sin(lonrad/2) + cos(lonrad/2), da)
 
-# A multilevel linear regression with random intercepts and random slopes.
-m1 = lmer(decimalLatitude ~ (1 + day | species) + day + sin(lonrad) + cos(lonrad) + sin(lonrad/2) + cos(lonrad/2), da)
+# A multilevel linear regression with random species intercepts and random day slopes for species.
+m2 = lmer(decimalLatitude ~ (1 + day | species) + day + sin(lonrad) + cos(lonrad) + sin(lonrad/2) + cos(lonrad/2), da)
+
+# Conduct a formal test that the day (within species) variance is significantly positive.
+# This requires us to fit two alternative models.
+m3 = lmer(decimalLatitude ~ (0 + day | species) + day + sin(lonrad) + cos(lonrad) + sin(lonrad/2) + cos(lonrad/2), da)
+m4 = lmer(decimalLatitude ~ (0 + day | species) + (1 | species) + day + sin(lonrad) + cos(lonrad) + sin(lonrad/2) + cos(lonrad/2), da)
+ss = exactRLRT(m3, m4, m1)
 
 # Estimate the marginal mean latitude at Detroit
 dp = head(da, 100)
 dp = dp %>% mutate(day=seq(min(da$day), max(da$day), length.out=100))
 dp = dp %>% mutate(lonrad=-pi*83/180) # Detroit longitude
-dp$yy = predict(m0, dp, re.form=NA)
+dp$yy = predict(m1, dp, re.form=NA)
 
 # Plot the marginal mean latitude
 plt = ggplot(dp, aes(x=day, y=yy)) + geom_line() + xlab("Day (x1000)") + ylab("Marginal mean latitude")
@@ -54,7 +64,7 @@ print(plt)
 # Get a day 0 value for each species, based on its longitude
 dx = da
 dx$day = 0
-dx$Latitude0 = predict(m1, dx, re.form=NA)
+dx$Latitude0 = predict(m2, dx, re.form=NA)
 dx = dx %>% group_by(species) %>% summarize(Latitude0=mean(Latitude0))
 
 # Plot species-level linear trends over this time range
@@ -63,7 +73,7 @@ day2 = as.numeric(max(da$day))
 
 # Use the predicted random effects to predict species-level
 # mean latitudes on the first and last day of the dataset.
-rr = ranef(m1)$species
+rr = ranef(m2)$species
 rr = as.data.frame(rr)
 rr$species = row.names(rr)
 row.names(rr) = NULL
