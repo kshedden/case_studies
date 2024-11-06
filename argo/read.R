f4acfa8939ce1e2a94e7b63cb5e180cdb9539293
kshedden <kshedden@umich.edu>
2024-11-06 11:08:53 -0500

    update

--- argo/read.R
+++ argo/read.R
@@ -1,5 +1,6 @@
 library(readr)
 library(dplyr)
+library(lubridate)
 
 # There is no R version of prep.py, you must run the
 # Python or julia prep script before using read.R.
@@ -7,21 +8,27 @@ library(dplyr)
 # This path must match the path in prep.py.
 qpath = "/home/kshedden/data/Teaching/argo/python"
 
+# The latitude where each profile was recorded
 lat = read_csv(file.path(qpath, "lat.csv.gz"))
 lat = as.vector(lat[,1])$Column1
 
+# The longitude where each profile was recorded
 lon = read_csv(file.path(qpath, "lon.csv.gz"))
 lon = as.vector(lon[,1])$Column1
 
+# The day of year on which a profile was recorded
 date = read_csv(file.path(qpath, "date.csv.gz"))
 date = as.vector(date[,1])$Column1
-day = date - min(date)
+day = yday(date)
 
+# The pressure (in decibars) for each point along a profile.
 pressure = read_csv(file.path(qpath, "pressure.csv.gz"))
 pressure = as.vector(pressure[,1])$Column1
 
+# The temperature profiles
 temp = read_csv(file.path(qpath, "temp.csv.gz"), col_names=F)
 temp = as.matrix(temp) %>% t
 
+# The salinity profiles
 psal = read_csv(file.path(qpath, "psal.csv.gz"), col_names=F)
 psal = as.matrix(psal) %>% t
--- argo/read.py
+++ argo/read.py
@@ -2,23 +2,27 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 
-# This script loads the data into several arrays
-# and vectors.  It is a separate script so that
-# it can be shared by several analysis scripts.
+# This script loads the Argo data into several arrays and vectors.  It is
+# a separate script from the notebooks so that it can be shared by several
+# notebooks.
 
 # This must match the variable "qpath" in the 'prep.py'
 # script.
 qpath = Path("/home/kshedden/data/Teaching/argo/python")
 
+# Latitude and longitude
 lat = np.loadtxt(qpath / "lat.csv.gz", skiprows=1)
 lon = np.loadtxt(qpath / "lon.csv.gz", skiprows=1)
 
+# The day of year that the profile was measured.
 date = np.loadtxt(qpath / "date.csv.gz", dtype="str", skiprows=1)
 date = pd.to_datetime(date)
-day = date - date.min()
-day = np.asarray([x.days for x in day])
+day = date.dayofyear
 
+# The pressure values (in decibars) for each point along a profile.
 pressure = np.loadtxt(qpath / "pressure.csv.gz", skiprows=1)
 
+# The temperature and salinity values, each column is a profile
+# obtained on a particular date and location.
 temp = np.loadtxt(qpath / "temp.csv.gz", delimiter=",").T
 psal = np.loadtxt(qpath / "psal.csv.gz", delimiter=",").T
