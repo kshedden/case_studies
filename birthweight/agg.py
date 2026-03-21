d5f094ba9d7403c5261300fc6ea0258f38647226
kshedden <kshedden@umich.edu>
2026-03-21 16:41:08 -0400

    update

--- birthweight/agg.py
+++ birthweight/agg.py
@@ -26,6 +26,14 @@ df["popsize"] = df["popsize"].astype(int)
 # Restrict to first born single births
 df = df.query("birthorder==1 & plurality==1")
 
+f1 = lambda x: stats.entropy(x.value_counts().values)
+f2 = lambda x: np.mean(x != "White")
+
+dr = df.groupby(["year", "loc"]).agg({"momrace": [f1, f2]})
+dr.columns = dr.columns.to_flat_index()
+dr = dr.rename(columns={("momrace", "<lambda_0>"): "momrace_entropy", ("momrace", "<lambda_1>"): "momrace_nonwhite"})
+dr = dr.reset_index()
+
 f1 = lambda x: np.sum(x <= 1500)
 f2 = lambda x: np.sum((x > 1500) & (x <= 2500))
 f3 = lambda x: np.sum(x > 2500)
@@ -38,5 +46,7 @@ da = da.reset_index()
 da = da.rename(columns={("birthweight", "<lambda_0>"): "vlb", ("birthweight", "<lambda_1>"): "lb", ("birthweight", "<lambda_2>"): "nb",
                 ("popsize", "median"): "popsize_median", ("momage", "mean"): "momage_mean",
                 ("dadage", "<lambda_0>"): "dadmissing_mean", ("dadage", "<lambda_1>"): "dadage_mean"})
+
+da = pd.merge(da, dr, on=["year", "loc"], how="left")
 
 da.to_csv(spath / "aggregated.csv.gz", compression="gzip", index=False)
